822c0acf5dca25dbd5b21bb8549e736c24aca5a9
OpusJoao <projetos.joao236@gmail.com>
2022-02-20 12:28:21 -0300

    WIP: adião de rotas de edição

--- src/categories/CategoriesController.js
+++ src/categories/CategoriesController.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { send } = require('express/lib/response');
+const { send, render } = require('express/lib/response');
 const Category = require('./Category');
 const slugify = require('slugify')
 const router = express.Router();
 const ROUTES = require('../../config/routes')
-const VIEW = require('../../config/views')
+const VIEW = require('../../config/views');
+const { route } = require('express/lib/application');
 
 router.get(ROUTES.CATEGORY.CREATE,(req,res) => {
     res.render(VIEW.CATEGORY.CREATE, {ROUTES: ROUTES})
@@ -21,6 +22,26 @@ router.get(ROUTES.CATEGORY.LIST,(req,res) => {
     })
 })
 
+router.get(ROUTES.CATEGORY.EDIT.GET,(req,res) => {
+    let { id } = req.params
+
+    if(isNaN(id)){
+        res.redirect(ROUTES.CATEGORY.LIST)
+    }
+
+    Category.findByPk(id).
+    then(category => {
+        if(category){
+            res.render(VIEW.CATEGORY.EDIT, {ROUTES:ROUTES, category: category})
+        }else{
+            res.redirect(ROUTES.CATEGORY.LIST)
+        }
+    }).catch(reason => {
+        console.error(`[ GET -> CategoriesController on ${ROUTES.CATEGORY.EDIT}] Categoria não encontrada ao editar`, reason)
+        res.redirect(ROUTES.CATEGORY.LIST)
+    })
+})
+// --------------------------------- POST ------------------------------
 router.post(ROUTES.CATEGORY.SAVE,(req,res) => {
     let title = req.body.title
     if(title && title != undefined && title != ""){
@@ -32,7 +53,7 @@ router.post(ROUTES.CATEGORY.SAVE,(req,res) => {
             res.redirect(ROUTES.CATEGORY.LIST)
         })
         .catch((reason)=>{
-            console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.SAVE}] Can't create a new category`, reason)
+            console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.SAVE}] Não foi possivel criar uma nova categoria`, reason)
             res.redirect(ROUTES.CATEGORY.CREATE)
         })
     }else{
@@ -53,12 +74,40 @@ router.post(ROUTES.CATEGORY.DELETE,(req,res) => {
             res.redirect(ROUTES.CATEGORY.LIST)
         })
         .catch((reason)=>{
-            console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.DELETE}] Can't delete the category`, reason)
-            res.redirect(ROUTES.CATEGORY.LIST)
+            console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.DELETE}] Não foi possivel deletar categoria`, reason)
+            res.render(VIEW.CATEGORY.LIST, {error: {title:'Erro ao deletar', body: 'Não foi possivel deletar categoria'}});
+
         })
     }else{
-        console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.DELETE}] id invalid`)
-        res.redirect(ROUTES.CATEGORY.LIST);
+        console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.DELETE}] ID de categoria inválido`)
+        res.render(VIEW.CATEGORY.LIST, {error: {title:'Erro ao deletar', body: 'ID de categoria inválido'}});
+    }
+})
+
+
+router.post(ROUTES.CATEGORY.EDIT.SAVE,(req,res) => {
+    let {id,title} = req.body
+    if(id && id != undefined && id != ""){
+        Category.update(
+            {
+                title: title,
+                slug: slugify(title),
+            },{
+            where:{
+                id:id
+            }
+        })
+        .then(()=>{
+            res.redirect(ROUTES.CATEGORY.LIST)
+        })
+        .catch((reason)=>{
+            console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.EDIT.SAVE}] Não foi possivel editar categoria`, reason)
+            res.render(VIEW.CATEGORY.LIST, {error: {title:'Erro ao editar', body: 'Não foi possivel editar categoria'}});
+
+        })
+    }else{
+        console.error(`[ POST -> CategoriesController on ${ROUTES.CATEGORY.EDIT.SAVE}] ID de categoria inválido`)
+        res.render(VIEW.CATEGORY.LIST, {error: {title:'Erro ao editar', body: 'ID de categoria inválido'}});
     }
 })
 
