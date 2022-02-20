7bb0135effeeeb11389f66532db93a5b87350b5a
OpusJoao <projetos.joao236@gmail.com>
2022-02-20 12:27:14 -0300

    WIP: Adição de rotas de deleção e edição

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,12 @@ const ROUTES = {
         CREATE : "/admin/category/new",
         SAVE : "/admin/category/",
         LIST: "/admin/categories/",
-        DELETE: "/admin/category/delete"
+        DELETE: "/admin/category/delete",
+        EDIT : {
+            GET: "/admin/category/edit/:id",
+            GET_WITHOUT_PARAM: "/admin/category/edit/",
+            SAVE: "/admin/category/edit/:id"
+        }
     },
     HOME : "/"
 }
