e2ea8d02afb7b44c9d54d751606064acd68c4e5f
OpusJoao <projetos.joao236@gmail.com>
2022-02-20 21:55:44 -0300

    WIP: Ajuste de timezone

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize')
 const connection = new Sequelize('guiapress','root','root',{
     host: 'mysql',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
+    timezone: "-03:00"
 })
 
 module.exports = connection 
