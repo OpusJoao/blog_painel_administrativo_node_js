03d8b1c74074fd6c434054217cfe86f06bc2ad48
OpusJoao <projetos.joao236@gmail.com>
2022-02-20 21:55:23 -0300

    WIP: Adição de novas rotas

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,17 @@
 const ROUTES = {
+    HOME : {
+        HOME: "/",
+        PAGE: "/page/:page",
+        PAGE_WITHOUT_PARAMS: "/page",
+        ARTICLE: "/article/:article_id/:article_slug",
+        ARTICLE_WITHOUT_PARAMS: "/article",
+        CATEGORY: "/category/:category_id/:category_slug",
+        CATEGORY_WITHOUT_PARAMS: "/category",
+        ERROR: "/error"
+    },
+    ADMIN : {
+        HOME: "/admin",
+    },
     CATEGORY : {
         CREATE : "/admin/category/new",
         SAVE : "/admin/category/",
@@ -10,7 +23,19 @@ const ROUTES = {
             SAVE: "/admin/category/edit/:id"
         }
     },
-    HOME : "/"
+    ARTICLE: {
+        CREATE : "/admin/article/new",
+        SAVE : "/admin/article/",
+        LIST: "/admin/articles/",
+        LIST_PAGE: "/admin/articles/page/:page",
+        LIST_PAGE_WITHOUT_PARAMS: "/admin/articles/page/",
+        DELETE: "/admin/article/delete",
+        EDIT : {
+            GET: "/admin/article/edit/:id",
+            GET_WITHOUT_PARAM: "/admin/article/edit/",
+            SAVE: "/admin/article/edit/:id"
+        }
+    },
 }
 
 module.exports = ROUTES
